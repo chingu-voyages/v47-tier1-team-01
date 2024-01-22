2cdc868cb3072ec9ef1029a02c75cd834ea6781e
jeru7 <john.ungab7@gmail.com>
2024-01-22 14:51:21 +0800

    added the close and open functions for the create-task modal

--- public/index.js
+++ public/index.js
@@ -1,10 +1,24 @@
-console.log('hello world')
+console.log("hello world");
 
 function showDiv(divId) {
-    // Hide all divs
-    document.querySelector('#date').style.display = 'none';
-    document.querySelector('#repeat-options').style.display = 'none';
+  // Hide all divs
+  document.querySelector("#date").style.display = "none";
+  document.querySelector("#repeat-options").style.display = "none";
 
-    // Show the selected div
-    document.getElementById(divId).style.display = 'block';
+  // Show the selected div
+  document.getElementById(divId).style.display = "block";
+}
+
+// modal functionalities
+function closeModal() {
+  document.querySelector(".createTaskModal").style.display = "none";
+  document.querySelector(".homepage").style.display = "flex";
+  //   document.querySelector(".overlay").style.display = "none";
+}
+
+//create task button (opens the create-task modal)
+function openCreateTask() {
+  document.querySelector(".createTaskModal").style.display = "flex";
+  document.querySelector(".homepage").style.display = "none";
+  //   document.querySelector(".overlay").style.display = "block";
 }
