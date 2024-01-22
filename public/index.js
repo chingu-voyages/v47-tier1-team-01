73d6916fcd7a93993c7f97a9ab47ff3b684f8ec2
jeru7 <john.ungab7@gmail.com>
2024-01-22 16:01:34 +0800

    added the functions for updating the select category and activity

--- public/index.js
+++ public/index.js
@@ -22,3 +22,54 @@ function openCreateTask() {
   document.querySelector(".homepage").style.display = "none";
   //   document.querySelector(".overlay").style.display = "block";
 }
+
+// pull the data in json file
+const jsonFile = "./assets/tasks-example.json";
+let jsonData;
+
+fetch(jsonFile)
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+    jsonData = data;
+    addCategories();
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
+// function to add the categories on the dropdown selection
+function addCategories() {
+  const categorySelection = document.querySelector("#category");
+  jsonData.forEach((category) => {
+    const option = document.createElement("option");
+    option.value = category.categoryName;
+    option.innerText = category.categoryName;
+    categorySelection.appendChild(option);
+  });
+}
+
+// function to update the activity selection based on the chosen category
+function updateActivitySelection() {
+  const categorySelection = document.querySelector("#category");
+  const currentCategory = categorySelection.value;
+  const activitySelection = document.querySelector("#activity");
+
+  const selectedCategory = jsonData.filter(
+    (category) => category.categoryName === currentCategory
+  );
+
+  const activities = selectedCategory[0].activityTypes.map(
+    (act) => act.activityName
+  );
+
+  activitySelection.innerHTML = `<option value="">Select Activity</option>`;
+
+  activities.forEach((activity) => {
+    const option = document.createElement("option");
+    option.value = activity;
+    option.text = activity;
+
+    activitySelection.append(option);
+  });
+}
