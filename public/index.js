125a36076ad50566422812e31339c8fbb36cb481
jeru7 <john.ungab7@gmail.com>
2024-01-24 19:20:21 +0800

    managed conflicts

--- public/index.js
+++ public/index.js
@@ -73,7 +73,6 @@ function updateActivitySelection() {
 }
 
 //Add new task START
-<<<<<<< HEAD
 const form = document.querySelector("form");
 const taskNameEl = document.querySelector("#task-name");
 const descriptionEl = document.querySelector("#description");
@@ -103,37 +102,6 @@ function saveTask(e) {
   //Form Validation
   const formHasError = formValidation();
   if (formHasError) return;
-=======
-const form = document.querySelector('form')
-const taskNameEl = document.querySelector('#task-name')
-const descriptionEl = document.querySelector('#description')
-const category = document.querySelector('#category')
-const categoryText = category.options[category.selectedIndex].text
-const activity = document.querySelector('#activity')
-const activityText = activity.options[activity.selectedIndex].text
-const dueDate = document.querySelector('#due-date')
-const date = document.querySelector('#date')
-const repeat = document.querySelector('#repeat')
-const repeatOptions = document.querySelector('#repeat-options')
-const priority = document.querySelector('#switch')
-const existingTasks = JSON.parse(localStorage.getItem('tasks')) || []
-const submit = document.querySelector('#save-task')
-
-submit.addEventListener('click', saveTask)
-console.log(submit)
-console.log(localStorage.tasks)
-
-function saveTask(e) {
-  e.preventDefault()
-  const category = document.querySelector('#category')
-  const categoryText = category.options[category.selectedIndex].text
-  const activity = document.querySelector('#activity')
-  const activityText = activity.options[activity.selectedIndex].text
-
-  //Form Validation
-  const formHasError = formValidation()
-  if (formHasError) return
->>>>>>> 46d816955d60bb9b388dbbb7fdf334055c89c3f8
 
   const object = {
     id: new Date().getTime(),
@@ -144,7 +112,6 @@ function saveTask(e) {
     priority: priority.checked,
   };
   if (dueDate.checked) {
-<<<<<<< HEAD
     object.deadline = date.value.split("-").reverse().join("/");
   } else if (repeat.checked) {
     object.deadline = getRepeatDays();
@@ -164,44 +131,18 @@ function getRepeatDays() {
     if (day.firstElementChild.checked) daysArray.push(day.firstElementChild.id);
   });
   return daysArray;
-=======
-    object.deadline = date.value.split('-').reverse().join('/')
-  } else if (repeat.checked) {
-    object.deadline = getRepeatDays();
-  }
-  existingTasks.push(object)
-  localStorage.setItem('tasks', JSON.stringify(existingTasks))
-  console.log(object)
-  form.reset()
-  populateTasks()
-  closeModal()
-}
-
-function getRepeatDays() {
-  let daysArray = []
-  ;[...repeatOptions.children].forEach((day) => {
-    if (day.firstElementChild.checked) daysArray.push(day.firstElementChild.id)
-  })
-  return daysArray
->>>>>>> 46d816955d60bb9b388dbbb7fdf334055c89c3f8
 }
 
 //retrieves a task and updates it. Not yet in use
 function getTaskById(taskId) {
-<<<<<<< HEAD
   const foundTask = existingTasks.find((task) => task.id === taskId);
   return foundTask;
-=======
-  const foundTask = existingTasks.find((task) => task.id === taskId)
-  return foundTask
->>>>>>> 46d816955d60bb9b388dbbb7fdf334055c89c3f8
 }
 //Add new task END
 
 //dinamically display tasks START
 
 function getTasksFromLocalStorage() {
-<<<<<<< HEAD
   const tasksJson = localStorage.getItem("tasks");
   return tasksJson ? JSON.parse(tasksJson) : [];
 }
@@ -213,32 +154,14 @@ function createTaskElement(task) {
   const para = document.createElement("p");
   para.classList.add("results__result");
   para.textContent = JSON.stringify(task);
-=======
-  const tasksJson = localStorage.getItem('tasks')
-  return tasksJson ? JSON.parse(tasksJson) : []
-}
-
-function createTaskElement(task) {
-  const div = document.createElement('div')
-  div.classList.add('results__container')
-
-  const para = document.createElement('p')
-  para.classList.add('results__result')
-  para.textContent = JSON.stringify(task)
->>>>>>> 46d816955d60bb9b388dbbb7fdf334055c89c3f8
 
   div.appendChild(para);
   return div;
 }
 
 function populateTasks() {
-<<<<<<< HEAD
   const tasks = getTasksFromLocalStorage();
   const resultsContainer = document.querySelector(".results");
-=======
-  const tasks = getTasksFromLocalStorage()
-  const resultsContainer = document.querySelector('.results')
->>>>>>> 46d816955d60bb9b388dbbb7fdf334055c89c3f8
 
   //makes sure tasks are not duplicated since this function is called both on page load and when SAVE button is clicked
   while (resultsContainer.firstChild) {
@@ -246,7 +169,6 @@ function populateTasks() {
   }
 
   if (tasks.length === 0) {
-<<<<<<< HEAD
     const noTasksPara = document.createElement("p");
     noTasksPara.textContent = "No tasks at this time";
     resultsContainer.appendChild(noTasksPara);
@@ -258,28 +180,11 @@ function populateTasks() {
   }
 }
 populateTasks();
-=======
-    const noTasksPara = document.createElement('p')
-    noTasksPara.textContent = 'No tasks at this time'
-    resultsContainer.appendChild(noTasksPara)
-  } else {
-    tasks.forEach((task) => {
-      const taskElement = createTaskElement(task)
-      resultsContainer.appendChild(taskElement)
-    })
-  }
-}
-populateTasks()
->>>>>>> 46d816955d60bb9b388dbbb7fdf334055c89c3f8
 
 //check functionality...a bit faulty...
 //check with no tasks
 
-<<<<<<< HEAD
 console.log(getTasksFromLocalStorage());
-=======
-console.log(getTasksFromLocalStorage())
->>>>>>> 46d816955d60bb9b388dbbb7fdf334055c89c3f8
 
 //dinamically display tasks END
 
@@ -309,10 +214,5 @@ function formValidation() {
     taskDescriptionErrorEl.style.visibility = "visible";
     formHasError = true;
   }
-<<<<<<< HEAD
-
   return formHasError;
-=======
-  return formHasError
->>>>>>> 46d816955d60bb9b388dbbb7fdf334055c89c3f8
 }
